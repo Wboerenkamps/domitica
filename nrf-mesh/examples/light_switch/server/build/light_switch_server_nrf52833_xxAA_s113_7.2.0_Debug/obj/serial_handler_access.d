649d658b25904581a11c6070225a90b064fc1389
“Wouter <wrj.boerenkamps@student.avans.nl>
2023-01-11 14:49:22 +0100

    between

---
+++ nrf-mesh/examples/light_switch/server/build/light_switch_server_nrf52833_xxAA_s113_7.2.0_Debug/obj/serial_handler_access.d
@@ -0,0 +1,89 @@
+build/light_switch_server_nrf52833_xxAA_s113_7.2.0_Debug/obj/serial_handler_access.o: \
+ C:\project\ domotica\nrf-mesh\mesh\serial\src\serial_handler_access.c \
+ include/serial_handler_access.h include/serial_packet.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 6.40/include/stdint.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 6.40/include/__SEGGER_RTL.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 6.40/include/__SEGGER_RTL_ConfDefaults.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 6.40/include/__SEGGER_RTL_Conf.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 6.40/include/__SEGGER_RTL_Arm_Conf.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 6.40/include/limits.h \
+ include/serial_cmd.h \
+ C:/nrf-mesh/nRF5_SDK_17.1.0_ddde560/components/softdevice/s113/headers/ble_gap.h \
+ C:/nrf-mesh/nRF5_SDK_17.1.0_ddde560/components/softdevice/s113/headers/nrf_svc.h \
+ C:/nrf-mesh/nRF5_SDK_17.1.0_ddde560/components/softdevice/s113/headers/nrf_error.h \
+ C:/nrf-mesh/nRF5_SDK_17.1.0_ddde560/components/softdevice/s113/headers/ble_hci.h \
+ C:/nrf-mesh/nRF5_SDK_17.1.0_ddde560/components/softdevice/s113/headers/ble_ranges.h \
+ C:/nrf-mesh/nRF5_SDK_17.1.0_ddde560/components/softdevice/s113/headers/ble_types.h \
+ C:/nrf-mesh/nRF5_SDK_17.1.0_ddde560/components/softdevice/s113/headers/ble_err.h \
+ ../../../mesh/core/api/nrf_mesh_defines.h include/nrf_mesh_serial.h \
+ ../../../mesh/core/api/nrf_mesh.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 6.40/include/stdbool.h \
+ ../../../mesh/core/include/timer.h \
+ C:/nrf-mesh/nRF5_SDK_17.1.0_ddde560/components/softdevice/s113/headers/ble.h \
+ C:/nrf-mesh/nRF5_SDK_17.1.0_ddde560/components/softdevice/s113/headers/ble_gap.h \
+ C:/nrf-mesh/nRF5_SDK_17.1.0_ddde560/components/softdevice/s113/headers/ble_l2cap.h \
+ C:/nrf-mesh/nRF5_SDK_17.1.0_ddde560/components/softdevice/s113/headers/ble_gatt.h \
+ C:/nrf-mesh/nRF5_SDK_17.1.0_ddde560/components/softdevice/s113/headers/ble_gattc.h \
+ C:/nrf-mesh/nRF5_SDK_17.1.0_ddde560/modules/nrfx/mdk/nrf.h \
+ C:/nrf-mesh/nRF5_SDK_17.1.0_ddde560/modules/nrfx/mdk/nrf52833.h \
+ C:/nrf-mesh/nRF5_SDK_17.1.0_ddde560/components/toolchain/cmsis/include/core_cm4.h \
+ C:/nrf-mesh/nRF5_SDK_17.1.0_ddde560/components/toolchain/cmsis/include/cmsis_version.h \
+ C:/nrf-mesh/nRF5_SDK_17.1.0_ddde560/components/toolchain/cmsis/include/cmsis_compiler.h \
+ C:/nrf-mesh/nRF5_SDK_17.1.0_ddde560/components/toolchain/cmsis/include/cmsis_gcc.h \
+ C:/nrf-mesh/nRF5_SDK_17.1.0_ddde560/components/toolchain/cmsis/include/mpu_armv7.h \
+ C:/nrf-mesh/nRF5_SDK_17.1.0_ddde560/modules/nrfx/mdk/system_nrf52833.h \
+ C:/nrf-mesh/nRF5_SDK_17.1.0_ddde560/modules/nrfx/mdk/nrf52833_bitfields.h \
+ C:/nrf-mesh/nRF5_SDK_17.1.0_ddde560/modules/nrfx/mdk/nrf52_to_nrf52833.h \
+ C:/nrf-mesh/nRF5_SDK_17.1.0_ddde560/modules/nrfx/mdk/nrf51_to_nrf52.h \
+ C:/nrf-mesh/nRF5_SDK_17.1.0_ddde560/modules/nrfx/mdk/compiler_abstraction.h \
+ C:/nrf-mesh/nRF5_SDK_17.1.0_ddde560/components/softdevice/s113/headers/ble_gatts.h \
+ ../../../mesh/core/api/nrf_mesh_defines.h \
+ ../../../mesh/core/api/nrf_mesh_config_core.h \
+ include/nrf_mesh_config_app.h \
+ C:/nrf-mesh/nRF5_SDK_17.1.0_ddde560/components/softdevice/s113/headers/nrf_sdm.h \
+ C:/nrf-mesh/nRF5_SDK_17.1.0_ddde560/components/softdevice/s113/headers/nrf_error_sdm.h \
+ C:/nrf-mesh/nRF5_SDK_17.1.0_ddde560/components/softdevice/s113/headers/nrf_soc.h \
+ C:/nrf-mesh/nRF5_SDK_17.1.0_ddde560/components/softdevice/s113/headers/nrf_error_soc.h \
+ ../../../mesh/prov/api/nrf_mesh_prov.h \
+ ../../../mesh/prov/api/nrf_mesh_config_prov.h \
+ ../../../mesh/prov/api/nrf_mesh_prov_types.h \
+ ../../../mesh/prov/api/nrf_mesh_prov_events.h \
+ ../../../mesh/prov/api/nrf_mesh_prov_bearer.h \
+ ../../../mesh/core/include/list.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 6.40/include/stddef.h \
+ C:/nrf-mesh/nRF5_SDK_17.1.0_ddde560/components/softdevice/s113/headers/nrf_error.h \
+ ../../../mesh/core/api/nrf_mesh_assert.h \
+ ../../../mesh/core/api/nrf_mesh.h \
+ C:/nrf-mesh/nRF5_SDK_17.1.0_ddde560/components/libraries/util/app_util.h \
+ C:/nrf-mesh/nRF5_SDK_17.1.0_ddde560/modules/nrfx/mdk/compiler_abstraction.h \
+ C:/nrf-mesh/nRF5_SDK_17.1.0_ddde560/components/libraries/util/nordic_common.h \
+ C:/nrf-mesh/nRF5_SDK_17.1.0_ddde560/components/softdevice/s113/headers/nrf52/nrf_mbr.h \
+ C:/nrf-mesh/nRF5_SDK_17.1.0_ddde560/components/softdevice/s113/headers/nrf_svc.h \
+ ../../../mesh/core/api/nrf_mesh_assert_gcc.h \
+ ../../../mesh/core/include/bitfield.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 6.40/include/string.h \
+ ../../../mesh/core/include/toolchain.h \
+ ../../../mesh/dfu/api/nrf_mesh_dfu.h \
+ ../../../mesh/core/api/nrf_mesh_config_core.h \
+ ../../../mesh/dfu/api/nrf_mesh_config_dfu.h \
+ ../../../mesh/dfu/api/nrf_mesh_dfu_types.h \
+ ../../../mesh/dfu/include/dfu_types_internal.h \
+ ../../../mesh/dfu/api/nrf_mesh_dfu_types.h \
+ ../../../mesh/core/include/timer_scheduler.h \
+ ../../../mesh/core/include/timer.h ../../../mesh/core/include/packet.h \
+ ../../../mesh/core/include/utils.h ../../../mesh/core/include/hal.h \
+ ../../../mesh/core/include/log.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 6.40/include/stdarg.h \
+ C:/nrf-mesh/nRF5_SDK_17.1.0_ddde560/components/libraries/util/nordic_common.h \
+ ../../../mesh/core/include/cache.h ../../../mesh/core/include/bl_if.h \
+ ../../../mesh/core/include/bl_info_types.h include/serial_types.h \
+ ../../../mesh/access/api/access.h \
+ ../../../mesh/access/api/device_state_manager.h \
+ ../../../mesh/core/include/toolchain.h include/serial_evt.h \
+ include/serial_cmd_rsp.h ../../../mesh/access/api/device_state_manager.h \
+ ../../../mesh/core/include/internal_event.h \
+ include/nrf_mesh_config_serial.h include/serial.h \
+ include/serial_status.h include/serial_cmd.h include/serial_cmd_rsp.h \
+ include/serial_handler_common.h ../../../mesh/access/api/access_config.h \
+ ../../../mesh/access/api/access.h \
+ ../../../mesh/access/api/access_status.h
